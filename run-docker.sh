1326f70e382580de3c5c54562eea52f1b3f61b61
Satsuki Akiba <anosatsuk124@gmail.com>
2025-06-04 16:38:15 +0900

    chore: update docker run command

--- run-docker.sh
+++ run-docker.sh
@@ -4,7 +4,8 @@
 docker run \
 	--volume "$(pwd)/feed_urls.txt:/app/feed_urls.txt" \
 	--volume "$(pwd)/.env:/app/.env" \
+	--volume "$(pwd)/public:/app/public" \
 	--publish 3000:3000 \
-	--tty \
-	--interactive \
-	voice-rss-summary
+	--name voice-rss-summary \
+	\
+	voice-rss-summary # --restart always \
